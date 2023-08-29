86f0a2451288fed627ded29bcc7f4cc059128645
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-29 09:51:41 +0200

    Add: Task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float n as argument and returns the floor
+    of the float.
+
+    Parameters:
+    n (float): the float to be floored.
+
+    Return:
+    int: the floored float.
+    """
+    return int(n)
