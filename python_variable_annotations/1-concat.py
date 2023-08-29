bba9439faab145aeb68c987d49ea216ef91c6d5c
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-29 09:47:52 +0200

    Add: Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains a function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concatenates two strings and returns the result as a string.
+
+    Parameters:
+    str1 (str): first string.
+    str2 (str): second string.
+
+    Return:
+    str: the concatenation of str1 and str2.
+    """
+    return str1 + str2
