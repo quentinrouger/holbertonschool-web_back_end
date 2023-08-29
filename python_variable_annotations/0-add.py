3b4cc482d652c1e40908f487b94ad6c559df2ce1
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-29 09:34:25 +0200

    Fix: Task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""This module contains a function that adds two floats"""
+"""This module contains a function that adds two floats."""
 
 def add(a: float, b: float) -> float:
     """
-    This function adds two floats and returns the sum as a float
+    This function adds two floats and returns the sum as a float.
     
     Parameters:
-    a: first float
-    b: second float
+    a (float): first float.
+    b (float): second float.
     
     Return: 
-    float: the sum of a and b
+    float: the sum of a and b.
     """
     return a + b
