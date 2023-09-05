e1f62befcf05edfdac92ed92c4b4cc83f22f862a
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-05 12:00:39 +0200

    Add: Task 12 and 13

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" 12. Log stats - new version """
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    documents = nginx_collection.count_documents({})
+    print("{} logs".format(documents))
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(
+            method,
+            nginx_collection.count_documents({"method": method})
+        ))
+
+    print("{} status check".format(
+        nginx_collection.count_documents({"$and": [{"path": "/status"},
+                                                   {"method": "GET"}]})
+    ))
