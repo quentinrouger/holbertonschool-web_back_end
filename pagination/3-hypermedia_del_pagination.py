ab2a30d9fec255a452b1b633be9067d56a182b76
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-08 12:08:09 +0200

    Add: Task 3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+Task 3: Deletion-resilient hypermedia pagination
 """
-
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
@@ -28,7 +27,7 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> dict[int, List]:
+    def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
@@ -39,23 +38,48 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
-        """Returns a dictionary with pagination parameters"""
-        assert isinstance(index, int) and isinstance(page_size, int)
-        assert 0 <= index < len(self.indexed_dataset())
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Retrieve a page of data from a dataset based on the given index and
+        page size.
+
+        Args:
+            index (int, optional): The starting index of the page to retrieve.
+                Default is None.
+            page_size (int, optional): The number of items to include in
+                the page. Default is 10.
+
+        Returns:
+            dict: A dictionary containing the index, data, page size,
+            and the next index for pagination purposes.
+        """
+        assert isinstance(index, int) and index >= 0
 
         indexed_dataset = self.indexed_dataset()
-        next_index = index + page_size
-        data = []
-        for i in range(index, next_index):
-            if indexed_dataset.get(i):
-                data.append(indexed_dataset[i])
-            else:
-                next_index += 1
 
-        return {
+        if index >= len(self.dataset()):
+            # If the index is outside the valid range, return an empty response
+            return {
+                'index': index,
+                'data': [],
+                'page_size': page_size,
+                'next_index': None
+            }
+
+        # Get the data for the current page using the index
+        data = [indexed_dataset.get(i, None) for i in
+                range(index, index + page_size)]
+
+        # Remove the None items (previously removed)
+        data = [item for item in data if item is not None]
+
+        next_index = index + page_size if len(data) > 0 else None
+
+        response = {
             'index': index,
-            'next_index': next_index,
-            'page_size': len(data),
-            'data': data
+            'data': data,
+            'page_size': page_size,
+            'next_index': next_index
         }
+
+        return response
