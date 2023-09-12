1d566cbcdc34e7199b33ce556bedef2dd59486c2
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-12 10:57:27 +0200

    Add: Task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let newArray = [];
+  for (let value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
