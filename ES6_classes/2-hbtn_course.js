670e22e7d4dc240257c45b6ebfe48cb6296a0902
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-12 13:42:10 +0200

    Modify: indentation task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -3,35 +3,35 @@ export default class HolbertonCourse {
     this.name = name;
     this.length = length;
     this.students = students;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    if (typeof name === 'string') {
+    this._name = name;
+    } else {
+    throw new TypeError('Name must be a string');
     }
-    get name() {
-      return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(length) {
+    if (typeof length === 'number') {
+        this._length = length;
+    } else {
+        throw new TypeError('Length must be a number');
     }
-    set name(name) {
-      if (typeof name === 'string') {
-        this._name = name;
-      } else {
-        throw new TypeError('Name must be a string');
-      }
+  }
+  get students() {
+    return this._students;
+  }
+  set students(students) {
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
+    this._students = students;
+    } else {
+    throw new TypeError('Students must be an array of strings');
     }
-    get length() {
-      return this._length;
-    }
-    set length(length) {
-        if (typeof length === 'number') {
-            this._length = length;
-        } else {
-            throw new TypeError('Length must be a number');
-        }
-    }
-    get students() {
-      return this._students;
-    }
-    set students(students) {
-      if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
-        this._students = students;
-      } else {
-        throw new TypeError('Students must be an array of strings');
-      }
-    }
+  }
 }
