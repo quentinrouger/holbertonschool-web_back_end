b95e1137178420bf2d44b1d53125aab8b15670f3
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-12 14:16:22 +0200

    Add: Task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -7,33 +7,31 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-
-  set name(name) {
-    if (typeof name !== 'string') {
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
       throw new TypeError('Name must be a string');
     }
-    this._name = name;
   }
-
   get length() {
     return this._length;
   }
-
-  set length(length) {
-    if (typeof length !== 'number') {
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
       throw new TypeError('Length must be a number');
     }
-    this._length = length;
   }
-
   get students() {
     return this._students;
   }
-
-  set students(students) {
-    if (!(students instanceof Array)) {
-      throw new TypeError('Students must be an array');
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && newStudents.every((student) => typeof student === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
-    this._students = students;
   }
 }
