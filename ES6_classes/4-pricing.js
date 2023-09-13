fa5c622a8d2dfad31121f59192c3bcf17d8c70ae
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-13 19:54:06 +0200

    Add: Task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === "number") {
+      this._amount = newAmount;
+    } else {
+      throw new TypeError("Amount must be a number");
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw new TypeError("Currency must be a Currency");
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== "number") {
+      throw new TypeError("Amount must be a number");
+    }
+    if (typeof conversionRate !== "number") {
+      throw new TypeError("Conversion rate must be a number");
+    }
+    return amount * conversionRate;
+  }
+}
