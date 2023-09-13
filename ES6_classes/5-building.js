0f9f5742d335163187ec0cdf0b969a45dec4531a
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-13 20:34:09 +0200

    Add: Task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,22 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    if (typeof newSqft === "number") {
-      this._sqft = newSqft;
-    } else {
-      throw new TypeError("Sqft must be a number");
-    }
   }
 }
