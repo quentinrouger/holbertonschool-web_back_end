843d372fdaa2e92986dfa07bad0b4de9b529f835
quentinrouger <quentin_rouger@hotmail.fr>
2023-10-02 11:09:02 +0200

    Add: Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(filename)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((values) => {
-      console.log(`${values[0].firstName} ${values[0].lastName} ${values[1].body}`);
-    })
-    .catch(() => Error);
+      const rejected = values.filter((value) => value.status === 'rejected');
+      const fulfilled = values.filter((value) => value.status === 'fulfilled');
+      fulfilled.forEach((value) => console.log(`${value.value.firstName} ${value.value.lastName}`));
+      rejected.forEach((value) => console.log(value.reason.message));
+    });
 }
