ed0eb2ddd97d664143ba5ea0beea6aab959b65e2
quentinrouger <quentin_rouger@hotmail.fr>
2023-10-02 08:21:32 +0200

    Add: Task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Simulate an API call, e.g., fetching data from an API
     setTimeout(() => {
       // For this example, we'll resolve the Promise with a sample response
-      const sampleResponse = { message: "Hello, this is your API response!" };
+      const sampleResponse = { message: 'Hello, this is your API response!' };
       resolve(sampleResponse);
     }, 2000); // Simulating a 2-second delay (adjust as needed)
   });
