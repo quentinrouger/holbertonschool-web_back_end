3560e42d39f339ed7b35c8c5c45b2d1e89c39306
quentinrouger <quentin_rouger@hotmail.fr>
2023-10-02 11:17:59 +0200

    Add: Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((values) => {
-      const rejected = values.filter((value) => value.status === 'rejected');
-      const fulfilled = values.filter((value) => value.status === 'fulfilled');
-      fulfilled.forEach((value) => console.log(`${value.value.firstName} ${value.value.lastName}`));
-      rejected.forEach((value) => console.log(value.reason.message));
-    });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
+    (value) => value.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : `${result.reason.name}: ${result.reason.message}` })),
+  ));
 }
