8eeca9bbd9ae1a0ad79a6c9d559855a691d073cd
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-13 09:48:55 +0100

    Fix: Task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -12,20 +12,14 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key and item:
+        """ Add an item to the cache """
+        if key is not None and item is not None:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                keys = list(self.cache_data.keys())
-                del self.cache_data[keys[0]]
-            else:
-                return None
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key and key in self.cache_data:
-            return self.cache_data.get(key)
-        else:
-            return None
+        """ Get an item by key """
+        return self.cache_data.get(key)
+
+
+if __name__ == "__main__":
+    my_cache = BasicCache()
