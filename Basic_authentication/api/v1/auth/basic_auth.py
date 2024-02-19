dce3bd8ff65c6098916b59e9f2ff25c2c777b4c9
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 15:36:22 +0100

    Add: Task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module to manage the API authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,18 @@ class BasicAuth(Auth):
 
         base64_credentials = authorization_header.split(" ")[1]
         return base64_credentials
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ Decode base64 authorization header
+        """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
