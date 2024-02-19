127acc5a4462cd5c502aea43582f5123d2d0b5ed
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 15:27:26 +0100

    Fix: pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,13 +7,16 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Class to manage the API authentication
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
         """ Extract base64 authorization header
         """
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or \
+                type(authorization_header) is not str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        
+
         base64_credentials = authorization_header.split(" ")[1]
         return base64_credentials
