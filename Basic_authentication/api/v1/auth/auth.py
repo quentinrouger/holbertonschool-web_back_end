68781fac72fa6803a1fb55b169fde6e3ea85af70
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 14:18:15 +0100

    Add: Task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Module to manage the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        return None
