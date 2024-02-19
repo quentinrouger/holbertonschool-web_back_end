7124518c25224314624a22bad98538a9de4e39d3
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 10:27:10 +0100

    Add: annotations

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,9 @@
 Write a function called filter_datum that returns the log message obfuscated
 """
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Returns the log message obfuscated
     """
