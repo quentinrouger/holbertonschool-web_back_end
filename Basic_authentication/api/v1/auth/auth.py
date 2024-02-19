4ade3f5c475047d78324d91d01391741c99cbb40
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 14:56:58 +0100

    Add: Task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,9 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Authorization header
         """
-        return None
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user
