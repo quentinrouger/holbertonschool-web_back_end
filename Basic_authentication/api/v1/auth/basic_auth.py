3d73af6e8bfd5142d8d36b8498754ac4c0e8c0dd
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 15:25:54 +0100

    Add: Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Class to manage the API authentication
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extract base64 authorization header
+        """
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        base64_credentials = authorization_header.split(" ")[1]
+        return base64_credentials
