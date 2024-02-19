8da05e25ef1d9e07b23aa6e1c90cd1acd9435201
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 10:29:51 +0100

    Refactor filter_datum function signature

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,13 @@ Write a function called filter_datum that returns the log message obfuscated
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
     """
     Returns the log message obfuscated
     """
