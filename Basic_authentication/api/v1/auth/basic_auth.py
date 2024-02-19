91cfeff098036713a2118b163495541550b25895
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 16:33:35 +0100

    Add: Task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,9 +66,8 @@ class BasicAuth(Auth):
         if not users:
             return None
 
-        user = users[0]
-
-        if not user.is_valid_password(user_pwd):
-            return None
+        for user in users:
+            if not user.is_valid_password(user_pwd):
+                return None
 
         return user
