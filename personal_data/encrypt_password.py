93ffc9eb8f476f30cbbd1a451ab28c233a4d9856
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-19 12:57:01 +0100

    Add: Task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     Returns a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Returns True if the password is valid, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
