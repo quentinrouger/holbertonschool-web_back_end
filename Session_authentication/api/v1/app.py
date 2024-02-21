844470f813f2a69c4372aa119d621d28955b6760
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-21 22:11:37 +0100

    Add: Task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -37,11 +37,13 @@ def before_request() -> None:
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
     current_user = auth.current_user(request)
     if current_user is None:
