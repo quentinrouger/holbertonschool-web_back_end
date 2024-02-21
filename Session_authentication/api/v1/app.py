f293e5e80f8570439dd12a483704b5621a7340b9
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-22 00:04:24 +0100

    Add: Task 7

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -4,8 +4,6 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -20,7 +18,7 @@ auth_type = getenv("AUTH_TYPE")
 if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth == "session_auth":
+elif auth_type == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 else:
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" Module to manage the API authentication
+""" Session authentication view for handling login requests.
 """
-from flask import jsonify, request, make_response
+from flask import jsonify, request
 from api.v1.views import app_views
 from models.user import User
 import os
@@ -19,19 +19,20 @@ def session_login() -> str:
     if not password:
         return jsonify({"error": "password missing"}), 400
 
-    user = User.search({'email': email})
+    user_list = User.search({'email': email})
 
-    if not user:
+    if not user_list:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if not user[0].is_valid_password(password):
+    user = user_list[0]
+
+    if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-    session_id = auth.create_session(user[0].id)
-
-    session_name = os.getenv('SESSION_NAME', '_my_session_id')
-    response = make_response(jsonify(user[0].to_json()))
+    session_id = auth.create_session(user.id)
+    session_name = os.getenv('SESSION_NAME')
+    response = jsonify(user.to_json())
     response.set_cookie(session_name, session_id)
 
     return response
