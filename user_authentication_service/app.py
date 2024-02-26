31752d8efab581b413f4989e8c0f2253f53b2414
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-26 19:25:50 +0100

    Add: Task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """App module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -14,6 +16,25 @@ def index() -> str:
       - Welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+    Return:
+      - User creation message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({"message": "email and password are required"}), 400
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
