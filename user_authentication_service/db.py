72763a93014d26eecde273672a4f29716bbe0750
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-26 12:52:54 +0100

    Add: Task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,7 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
+        """ Add user to the database
         """
         if not email or not hashed_password:
             return None
