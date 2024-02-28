dbef4aabfffbd118487730035cc4229e78b75fa7
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-28 14:46:16 +0100

    Add: Task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -55,5 +55,20 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /sessions
+    Return:
+      - Logout message
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return jsonify({"message": "Bienvenue"}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
