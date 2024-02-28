55d17a3a438a4776ae3b0554820fa794d3588808
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-28 12:22:42 +0100

    Add: Task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -26,3 +26,11 @@ class Auth:
         if user:
             raise ValueError(f"User {email} already exists")
         return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login
+        """
+        user = self._db.find_user_by(email=email)
+        if user and bcrypt.checkpw(password.encode(), user.hashed_password):
+            return True
+        return False
