e45a1839a6a37f9fbd01679661927482920c8127
quentinrouger <quentin_rouger@hotmail.fr>
2024-02-28 12:55:37 +0100

    Add: Task 13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -59,3 +59,9 @@ class Auth:
         if user:
             return user
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session
+        """
+        self._db.update_user(user_id, session_id=None)
+        return None
