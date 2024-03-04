f76ff95c262f913b4d3ab87a4538d1fefc94717e
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-04 11:40:48 -0500

    Add: Task 0

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""File for unit tests
+"""
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test the access_nested_map function
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test the access_nested_map function
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
