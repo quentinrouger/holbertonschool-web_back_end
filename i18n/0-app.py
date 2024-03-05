417acfc0fce3ec6326fe2680ce2430333a0b0504
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-05 10:25:03 -0500

    Add: Task 0

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
