0a90a4a3b19b435c9ede33d978efaf5cc0a8fd63
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-05 11:01:46 -0500

    Add: babel.localeselector

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -18,15 +18,16 @@ class Config():
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """Return index.html"""
     return render_template('2-index.html')
-
-
-def get_locale():
-    """Get locale"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
