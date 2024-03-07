94ffd909e90aa0f3749ebcaa3791a2f989a0fc2f
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-06 23:08:52 -0500

    Add: Task 4

---
+++ i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Route module for the API"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    if 'locale' in request.args:
+        requested_locale = request.args['locale']
+
+        if requested_locale in app.config['LANGUAGES']:
+            return requested_locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Return index.html"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
