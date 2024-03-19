de1cb038b86033a8aec5bcf55433718d73cd37a2
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-19 15:40:45 +0100

    Fix: Task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- Task 2 Best band ever
 -- script that ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT origin, SUM(nb_fans) AS total_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
