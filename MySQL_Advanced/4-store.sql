647b67a706fbe56422f97acf4fa29929e6f4576e
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-20 10:30:29 +0100

    Add: Task 4

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,16 @@
+-- Task 4 buy buy buy
+-- script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ;
