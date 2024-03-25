767ced9358faa67719e1388bcce9b562c8d6bffa
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-25 13:02:22 +0100

    Add: Task 0

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+    it('should return the sum of rounded numbers', () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('should return the sum of rounded numbers', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('should return the sum of rounded numbers', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('should return the sum of rounded numbers', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+});
