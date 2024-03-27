927f8a1913a61a6e75cd4525c12fa4499c5f3292
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-27 12:32:51 +0100

    Add: Task 3

---
+++ queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Promisify the get function
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const reply = await getAsync(schoolName);
+    console.log(reply);
+  } catch (error) {
+    console.error(`Error fetching value for ${schoolName}: ${error.message}`);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
