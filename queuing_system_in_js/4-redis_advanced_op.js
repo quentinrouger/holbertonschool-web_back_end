a4d58d245b5b6f6af10f72e891f24dd27dc0f623
quentinrouger <quentin_rouger@hotmail.fr>
2024-03-27 12:37:45 +0100

    Add: Task 4

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,29 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Create Hash
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+// Display Hash
+client.hgetall('HolbertonSchools', (err, reply) => {
+  if (err) {
+    console.error(`Error fetching hash: ${err.message}`);
+  } else {
+    console.log('Hash stored in Redis:');
+    console.log(reply);
+  }
+});
